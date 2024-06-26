1b9646d182fbdf4bcaffbd0119fc35dd7b74200a
ramanandkrgupta <rgupta1842005@gmail.com>
2024-06-27 04:05:22 +0530

    sigup eroor handle

--- controllers/user.js
+++ controllers/user.js
@@ -3,13 +3,15 @@ const User = require('../models/user')
 const { setUser } = require('../service/auth')
 async function handleUserSigup(req, res) {
     const { name, password, email } = req.body;
+    const exitedUser = await User.findOne({email});
+    if (exitedUser) return res.status(400).json({error: "User already"})
     await User.create({
         name,
         email,
         password,
 
     });
-    return res.redirect("/")
+    return res.redirect("/login")
 }
 
 async function handleUserLogin(req, res) {
